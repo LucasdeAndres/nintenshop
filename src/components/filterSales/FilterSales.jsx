d84a3624671c278fadccb3a8d7c119e9034678a0
LucasdeAndres <lucas.de.andres.1@gmail.com>
2023-06-07 23:58:46 -0300

    quick commit

---
+++ src/components/filterSales/FilterSales.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import discounts from "../../assets/jsons/discounts.json"
+import { HiMagnifyingGlass } from 'react-icons/hi'
+
+export const FilterSales = () => {
+  return (
+    <div className='filter_sale'>
+        <input></input>
+        <select name='discount'>
+         <HiMagnifyingGlass />   
+            <option value="">Select Value</option>
+            {discounts.map(discount =>{
+                return <option value={discount.value}>{discount.name}</option>
+            })}
+        </select>
+        <button></button>
+    </div>
+  )
+}
